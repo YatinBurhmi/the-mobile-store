08d26972fb3d5d6f61b983a35a1213713282c1b8
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-02-25 15:43:31 +0530

    Added PropTypes to Validate the data

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import Product from './Product'
-import Title from './Title'
-import {ProductConsumer} from '../context';
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-    state = {
-        products:[]
-    }
-    
-    render() {
-        return (
-            <React.Fragment>
-                <div className="py-5">
-                    <div className="container">
-                        <Title name="our" title="products" ></Title>
-                        <div className="row">
-                            <ProductConsumer>
-                                {value => {
-                                    return value.products.map(product => {
-                                        return <Product key={product.id} product={product} />
-                                    })
-                                }}
-                            </ProductConsumer>
-                        </div>
-                    </div>
-                </div>
-               {/* <Product/> */}
-            </React.Fragment>
-        )
-    }
+  state = {
+    products: []
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products"></Title>
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </div>
+        {/* <Product/> */}
+      </React.Fragment>
+    );
+  }
 }
